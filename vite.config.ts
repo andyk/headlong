2d56a8bfa88767ad862fa859d74521ba4e6b0aa7
Andy Konwinski <andykonwinski@gmail.com>
2024-02-19 15:50:41 -0800

    remove unecessary changes from main branch

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   define: {
     "import.meta.env.OPENAI_API_KEY": JSON.stringify(process.env.OPENAI_API_KEY),
+    // "import.meta.env.OPENAI_ORG": JSON.stringify(process.env.OPENAI_ORG),  // This environment var isn't actually being used
     "import.meta.env.HF_API_KEY": JSON.stringify(process.env.HF_API_KEY),
     "import.meta.env.HF_LLAMA_ENDPOINT": JSON.stringify(process.env.HF_LLAMA_ENDPOINT),
     "import.meta.env.SUPABASE_SERVICE_ROLE_KEY_HEADLONG": JSON.stringify(
