b43cea09e234f4eccb61c8f38bf0f8a34dae1d4e
Andy Konwinski <andykonwinski@gmail.com>
2024-02-23 11:03:31 -0800

    WIP: bug w/ bashServer using '-i' interactive shell causes it to get SIGINT - try node-pty instead

--- packages/env/bashServer.ts
+++ packages/env/bashServer.ts
@@ -1,143 +1,133 @@
-import { spawn } from 'child_process';
-import net from 'net';
-import { maybeMultipartFormRequestOptions } from 'openai/uploads';
+import { createServer, Socket } from 'net';
+import { IPty, spawn } from 'node-pty'; // Import spawn from node-pty
 
 const bashServerPort = Number(process.env.BASH_SERVER_PORT) || 3031;
 
+interface Shell {
+  proc: IPty;
+  history: string;
+}
+
 interface Env {
-    shells: {
-        [id: string]: {
-            proc: any,
-            history: string
-        }
-    };
-    activeShellID: string | null;
+  shells: { [id: string]: Shell };
+  activeShellID: string | null;
 }
 
 const env: Env = {
-    shells: {},
-    activeShellID: null
+  shells: {},
+  activeShellID: null,
 };
 
-const server = net.createServer();
+const server = createServer();
 
-const sockets: net.Socket[] = [];
+const sockets: Socket[] = [];
 
 function writeToSockets(msg: string) {
-    for (const socket of sockets) {
-        socket.write(msg);
-    }
+  sockets.forEach((socket) => {
+    socket.write(msg);
+  });
 }
 
 function newShell(payload: any) {
-    const {
-        shellID = undefined, shellPath = "/bin/bash", shellArgs = []
-    } = payload;
-    const id = shellID ? shellID : "shell-" + Math.random().toString(36).substring(7);
-    const augmentedShellArgs = [
-        '--rcfile',
-        './.bashrc',
-        ...shellArgs,
-        "-i"
-    ];
-    console.log("calling spawn with: ", shellPath, augmentedShellArgs);
-    env.shells[id] = {
-        proc: spawn(
-            `${shellPath}`,
-            augmentedShellArgs,
-            { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] }
-        ),
-        history: ''
-    };
-    env.activeShellID = id;
-    env.shells[id].proc.stdin?.write('\n');
-    writeToSockets(`observation: created shell with ID ${id} and made it active shell.`);
+  const { shellID, shellPath = '/bin/bash', shellArgs = [] } = payload;
+  const id = shellID || `shell-${Math.random().toString(36).substring(7)}`;
 
-    // Relay messages from the subprocess to the socket
-    env.shells[id].proc.stdout?.on('data', (data) => {
-        console.log(`observation: shell ${id}:\n` + data);
-        writeToSockets(`observation: shell ${id}:\n` + data);
-    });
-    env.shells[id].proc.stderr?.on('data', (data) => {
-        console.log(`observation: shell ${id}:\n` + data);
-        writeToSockets(`observation: shell ${id}:\n` + data);
-    });
-    env.shells[id].proc.on('exit', (data) => {
-        console.log(`observation: shell '${id}' exited.`);
-        writeToSockets(`observation: shell '${id}' exited.`);
-        //TODO: mark shell as exited in env.shells
-    });
-    env.shells[id].proc.on('close', (signal) => {
-        console.log(`observation: shell '${id}' terminated due to receipt of signal ${signal}`);
-        writeToSockets(`observation: shell '${id}' terminated due to receipt of signal ${signal}`);
-    });
+  // Using node-pty to spawn the shell
+  env.shells[id] = {
+    proc: spawn(shellPath, shellArgs, {
+      name: 'xterm-color',
+      cwd: process.cwd(),
+      env: process.env,
+    }),
+    history: '',
+  };
+  env.activeShellID = id;
+
+  writeToSockets(`observation: created shell with ID ${id} and made it active shell.`);
+
+  // Relay messages from the subprocess to the socket
+  env.shells[id].proc.onData((data) => {
+    writeToSockets(`observation: shell ${id}:\n${data}`);
+  });
+
+  env.shells[id].proc.onExit(({ exitCode, signal }) => {
+    writeToSockets(`observation: shell '${id}' exited with code ${exitCode}, signal ${signal}.`);
+    // Cleanup shell from env.shells when it exits
+    delete env.shells[id];
+    if (env.activeShellID === id) {
+      env.activeShellID = null; // Reset active shell ID if the exited shell was active
+    }
+  });
 }
 
 function runCommand(payload: any) {
-    if (env.activeShellID === null) {
-        writeToSockets("observation: there are no shells open");
-    } else {
-        const { command } = payload;
-        env.shells[env.activeShellID].proc.stdin?.write(command + '\n');
-    }
+  if (!env.activeShellID) {
+    writeToSockets('observation: there are no shells open.');
+    return;
+  }
+  const { command } = payload;
+  env.shells[env.activeShellID].proc.write(`${command}\n`);
 }
 
 function switchToShell(payload: any) {
-    const { id } = payload;
-    if (id in env.shells) {
-        env.activeShellID = id;
-        writeToSockets(`observation: switched to shell '${id}'`);
-    } else {
-        writeToSockets(`observation: shell '${id}' does not exist`);
-    }
+  const { id } = payload;
+  if (env.shells[id]) {
+    env.activeShellID = id;
+    writeToSockets(`observation: switched to shell '${id}'.`);
+  } else {
+    writeToSockets(`observation: shell '${id}' does not exist.`);
+  }
 }
 
-function whichShellActive(payload: any) {
-    if (env.activeShellID === null) {
-        writeToSockets(`observation: there are no shells open`);
-    } else {
-        writeToSockets(`observation: active shell is '${env.activeShellID}'`);
-    }
+function whichShellActive() {
+  if (!env.activeShellID) {
+    writeToSockets('observation: there are no shells open.');
+  } else {
+    writeToSockets(`observation: active shell is '${env.activeShellID}'.`);
+  }
 }
 
 server.on('connection', (socket) => {
-    console.log('bashServer: client connected')
-    sockets.push(socket);
-    // Relay messages from the socket to the subprocess
-    socket.on('data', (data) => {
-        console.log("received: ", data.toString());
-        const msg = JSON.parse(data.toString());
-        const { type, payload = {} } = msg;
-        if (type === 'newShell') {
-            newShell(payload);
-        } else if (type === 'runCommand') {
-            runCommand(payload);
-        } else if (type === 'switchToShell') {
-            switchToShell(payload);
-        } else if (type === 'whichShellActive') {
-            whichShellActive(payload);
-        } else {
-            console.log("received unrecognized type from client: ", type);
-        }
-    });
-    socket.on('close', () => {
-        console.log('a client disconnected');
-        const index = sockets.indexOf(socket);
-        if (index !== -1) {
-            sockets.splice(index, 1);
-        }
-    });
-});
+  console.log('bashServer: client connected');
+  sockets.push(socket);
 
+  socket.on('data', (data) => {
+    console.log('received:', data.toString());
+    const msg = JSON.parse(data.toString());
+    const { type, payload = {} } = msg;
+    switch (type) {
+      case 'newShell':
+        newShell(payload);
+        break;
+      case 'runCommand':
+        runCommand(payload);
+        break;
+      case 'switchToShell':
+        switchToShell(payload);
+        break;
+      case 'whichShellActive':
+        whichShellActive();
+        break;
+      default:
+        console.log('received unrecognized type from client:', type);
+    }
+  });
+
+  socket.on('close', () => {
+    console.log('a client disconnected');
+    const index = sockets.indexOf(socket);
+    if (index !== -1) {
+      sockets.splice(index, 1);
+    }
+  });
+});
 
 server.listen(bashServerPort, () => {
   console.log(`bashServer listening on port ${bashServerPort}`);
 });
 
-console.log("done running listen. registering process.on('SIGTERM')")
-// Listen for SIGTERM signal
+console.log("done running listen. registering process.on('SIGTERM')");
 process.on('SIGTERM', () => {
-    console.log('SIGTERM signal received. Shutting down...');
-    // Perform any cleanup operations here
-    process.exit(0);
+  console.log('SIGTERM signal received. Shutting down...');
+  process.exit(0);
 });
