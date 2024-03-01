6176fd23349c5a3f913dce2d493553812e56f01e
Robert Carroll <recarroll@gmail.com>
2024-03-01 11:11:32 -0800

    Fixed tokenizer loading

--- packages/webapp/src/huggingface.ts
+++ packages/webapp/src/huggingface.ts
@@ -1,10 +1,12 @@
 import { HfInferenceEndpoint } from '@huggingface/inference'
-import { AutoTokenizer } from "@xenova/transformers";
+import { AutoTokenizer, env } from "@xenova/transformers";
 const hfLlamaEndpoint = import.meta.env.VITE_HF_LLAMA_ENDPOINT;
 const hfApiKey = import.meta.env.VITE_HF_API_KEY;
 console.log("hfApiKey: ", hfApiKey);
 console.log("hfLlamaEndpoint: ", hfLlamaEndpoint);
 const hf = new HfInferenceEndpoint(hfLlamaEndpoint, hfApiKey);
+process.env.HF_TOKEN = hfApiKey;
+env.allowLocalModels = false;  // Need to set this otherwise, loading the remote model fails!?
 const tokenizer = await AutoTokenizer.from_pretrained('NousResearch/Llama-2-7b-chat-hf');
 // Accessing gated tokenizers doesn't seem to work. Why?
 // const tokenizer = await AutoTokenizer.from_pretrained('recarroll/headlong_llama2_7b_v3_prompted');
